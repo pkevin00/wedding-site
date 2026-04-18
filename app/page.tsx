4de0d024cbdd8c7d24aada0898ff20788083fbb7
pkevin00 <kevinpratama@outlook.fr>
2026-04-18 11:32:28 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,22 @@ export default function Home() {
   return (
     <main className="min-h-screen text-white bg-neutral-950 flex justify-center px-4">
 
-      {!opened && (
-        <div className="fixed inset-0 bg-black flex items-center justify-center z-50">
-          <button
-            onClick={() => setOpened(true)}
-            className="bg-white text-black px-6 py-3 rounded-full"
-          >
-            Buka Undangan
-          </button>
-        </div>
-      )}
-
       {opened && (
-        <div className="w-full max-w-md py-10">
-          <h1 className="text-center text-3xl">Aldi & Sinta</h1>
-        </div>
-      )}
+  <button
+    onClick={toggleMusic}
+    className="
+      fixed bottom-5 right-5
+      bg-white/10 border border-white/20
+      backdrop-blur-lg
+      p-3 rounded-full
+      text-sm
+      opacity-70 hover:opacity-100
+      transition
+    "
+  >
+    {playing ? "🔊" : "🔇"}
+  </button>
+)}
 
     </main>
   );
